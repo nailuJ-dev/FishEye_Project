ea9915ee5301043fd23431c1979fcc84b637c7b0
nailuJ <78428426+nailuJ-dev@users.noreply.github.com>
2021-10-14 12:14:19 +0200

    homepageBuilder and scrollButton added

---
+++ js/homepage/homepageBuilder.js
@@ -0,0 +1,32 @@
+// IMPORT OTHERS FUTURE ELEMENTS (FILTERS & SCROLL BUTTON)
+
+import scrollButton from "./scrollButton";
+
+// DISPLAY PHOTOGRAPHERS
+export default class homepageBuilder {
+
+    // Create photographers section in html index, call function for filters and feature button
+    showPhotographers(json) {
+        let photographers = json.photographers;
+        photographers.map(photograph => {
+            let sectionPartPhotographers = document.getElementById('photographers');
+            let articlePartPhotographers = document.createElement('article');
+            articlePartPhotographers.className = photograph.tags.join(' ') + ' articlePhoto';
+
+            let templateModelPhotographer = `
+            <a href='photographer-page.html?id={photograph.id}' title='{photograph.name}'>
+                <img src='{photograph.portrait}' alt='{photograph.alt}'>
+                <h2 class='name>{photograph.name}</h2>
+            </a>
+            <p class='location'>{photograph.city}, {photograph.country}</p>
+            <p class='tagline'>{photograph.tagline}</p>
+            <p class='price'>{photograph.price}€/jour</p>
+            <ul class='filters'>{photograph.tags.map(function (tag) => '<li data-filter='{tag}'>' + '#{tag}' + '</li>').join(' ')}</ul>
+            `
+            sectionPartPhotographers.appendChild(articlePartPhotographers);
+            articlePartPhotographers.innerHTML = templateModelPhotographer;
+        })
+        new scrollButton().scrollBtn();
+        // add function for filters
+    }
+}
---
+++ js/homepage/scrollButton.js
@@ -0,0 +1,15 @@
+export default class scrollButton {
+    // Get position on screen to bring the user back to the top of the website page
+    scrollBtn() {
+        window.addEventListener('scroll', () => {
+            let button = document.getElementById('main-photographer-button');
+            let axisY = window.scrollY;
+
+            if (axisY >= 130) {
+                button.style.display = 'block';  // check once css is done !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            } else {
+                button.style.display = 'none';
+            }
+        });
+    }
+}
