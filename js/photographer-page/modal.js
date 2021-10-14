33590b2ec5a0f2c4c5f3b967a73bd4e65f29aeb5
nailuJ <78428426+nailuJ-dev@users.noreply.github.com>
2021-10-14 15:20:14 +0200

    base for modal.js added

---
+++ js/photographer-page/modal.js
@@ -0,0 +1,38 @@
+// INTERACTING WITH MODAL WHEN CLICKING ON CONTACT ME BUTTON
+
+export default class Modal {
+    // Events when clicking
+    modal(json) {
+        const modalButton = document.getElementById('photographer-contact'); // add this ID in the photagrapher page builder
+        const closeButton = document.getElementsByClassName('close-form-icon'); // select the cross icon
+
+        if (modalButton) {
+            modalButton.addEventListener('click', this.launchModal);
+            this.formPhotographerName(json);
+        }
+        if (closeButton) {
+            closeButton[0].addEventListener('click', this.closeModal);
+        }
+    };
+
+    // Launch Modal
+    openModal() {
+        const modalBg = document.getElementById('dialog-contact-form');
+        modalBg.style.display = 'block';  // stay alert on the css name for css file
+    }
+
+    // Close Modal
+    closeModal() {
+        const modalBg = document.getElementById('dialog-contact-form');
+        modalBg.style.display = 'none';
+    };
+
+    // Add photographer names in form
+    formPhotographerName(json) {
+        let id = window.location.search.split('id=')[1];
+        let photographers = !id ? json : json.filter(photographer => photographer.id == id);
+        let photographerName = document.getElementById('photographer-name-for-form');
+        let photographerNameTemplate = '{photographers[0].name}';
+        photographerName.innerHTML = photographerNameTemplate;
+    };
+};
