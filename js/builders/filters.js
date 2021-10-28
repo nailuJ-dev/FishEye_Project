67ee3deeaf93c1523cd841f6d4d9a207fb097a62
nailuJ <78428426+nailuJ-dev@users.noreply.github.com>
2021-10-28 18:10:59 +0200

    filters need some fixes

--- js/builders/filters.js
+++ js/builders/filters.js
@@ -3,11 +3,13 @@ export default class Filter {
     // Get filters activated
     filterActived() {
         let getFilters = document.querySelector('ul');
-        let articlePart = document.querySelectorAll('.articlePartPhotographers');
+        let articlePart = document.querySelectorAll('.photographerArticle');
 
         // Events on click li elements
         getFilters.addEventListener('click', event => {
+            console.log('merde')
             let classValue = event.target.classList.value;
+            console.log('yes')
             if (classValue.indexOf('activated')) {
                 event.target.classList.add('activated')
             } else {
@@ -19,24 +21,26 @@ export default class Filter {
 
     // Grab filters with activated class and place its in an array
     getActivatedFilters() {
-        let activatedFilters = document.querySelectorAll('ul li.activated');
+        let activatedFilters = document.querySelectorAll('li .activated');
         let filtersActivated = [];
-        activatedFilters.forEach(function (selectedFilters) {
-            filtersActivated.push(selectedFilters.getAttribute('data-filter'));
+        activatedFilters.forEach(function (activatedFilter) {
+            filtersActivated.push(activatedFilter.getAttribute('data-filter'));
         });
         return filtersActivated;
     };
 
     // Check if filter = article class
+    // Do it with 'if tags in filtersActivated ... re-use this filters.filter(value => tagSelected.includes(value)) for a function 
     checkAllFilters(article) {
         let filters = this.getActivatedFilters();
         let classValues = article.classList.value;
         let tagSelected = classValues.split(' ');
         let crossCheck = filters.filter(value => tagSelected.includes(value));
-        return filters.length == crossCheck.length;
+        return filters.length === crossCheck.length;
     };
 
     // Show and Hide articles
+    // Do it with a function that includes upper comments  with a forEach loop and the keyboard binding Enter
     sortingDomArticles(articlePart) {
         articlePart.forEach((article) => {
             if (this.checkAllFilters(article)) {
--- js/photographer-page/profileCardPhotographer.js
+++ js/photographer-page/profileCardPhotographer.js
@@ -13,7 +13,7 @@ export default class ProfilePhotographers {
                 <h2>${photographers[0].name}</h2>
                 <p class="photographers-city">${photographers[0].city}, ${photographers[0].country}</p>
                 <p class="photographers-tagline">${photographers[0].tagline}</p>
-                <p >${photographers[0].tags.map(tag => `<a class="photographers-tags" href="index.html">#${tag}</a>`).join(" ")}</p>
+                <p >${photographers[0].tags.map(tag => `<a class="photographers-tags" href="index.html">#${tag}</a>`).join(' ')}</p>
             </div>
             <button id="photographers-contact" title='Contact Me'>Contactez-moi</button>
             <a href='#' title='${photographers[0].alt}'><img src="${photographers[0].portrait}" alt="${photographers[0].alt}"></a>
